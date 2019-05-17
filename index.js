b94760fdda52536d2f73a48d4bbfb9af8b4b96b8
Danish Backer <danish@blowoutandgo.com>
2019-05-17 05:45:38 +0400

    First commit

---
+++ index.js
@@ -0,0 +1,59 @@
+//dependencies required for the app
+const express = require("express");
+const http = require('http');
+const routes = require('./routes');
+const bodyParser = require("body-parser");
+const csv = require('fast-csv');
+const multer  = require('multer');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+//render css files
+// app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+
+//post route for uploading file to s3 bucket.
+app.post('/upload', routes.upload);
+
+// Set temp storage
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'tmp');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + '.csv');
+    }
+});
+   
+let uploadlocal = multer({ storage: storage });
+
+app.post('/parse', uploadlocal.single('soh'), (req, res, next) => {
+    const file = req.file;
+    if (!file) {
+      const error = new Error('Please upload soh.csv file');
+      error.httpStatusCode = 400;
+      return next(error);
+    }
+    const csvRows = [];
+    csv.fromPath(req.file.path, { headers: false, ignoreEmpty: true })
+    .on("data", function (data) {
+        csvRows.push(data); // push each row
+    })
+    .on("end", function () {
+        res.render("parse", { title: 'Parser', rows: csvRows, path: req.file.path });
+    });
+});
+
+app.get('/view', routes.view);
+
+// app.post('/removetask', routes.remove);
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,73 @@
+const csv = require('fast-csv'),
+    fs = require('fs'),
+    AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./s3_config.json');
+const s3 = new AWS.S3();
+const myBucketName = 'atg-test-16052019';
+
+exports.index = function(req, res){
+    let params = { 
+        Bucket: myBucketName
+    };
+    s3.listObjects(params, function (err, data) {
+        if (err) {
+            // console.log("error", err);
+            // return 'There was an error viewing your album: ' + err.message
+            return res.send({ "error": err });
+        }
+        // console.log(data.Contents,"<<<all content");
+        let uploadedFiles = [];
+        data.Contents.forEach(function(obj,index){
+            // console.log(obj.Key,"<<<file path")
+            uploadedFiles.push(obj.Key);
+        });
+        res.render("index", { title: 'Home', uploadedFiles: uploadedFiles });
+    });
+};
+
+exports.view = function (req, res) {
+    let params = { 
+        Bucket: myBucketName,
+        Key: req.query.file
+    };
+    s3.getObject(params, function (err, data) {
+        if (err) {
+            console.log("error", err);
+            return res.send({ "error": err });
+        }
+        // let content = data.Body.toString('utf8');
+        // console.log(content,"<<<all content");
+        const csvRows = [];
+        csv.fromString(data.Body.toString('utf8'), { headers: false, ignoreEmpty: true })
+        .on("data", function (data) {
+            csvRows.push(data); // push each row
+        })
+        .on("end", function () {
+            res.render("view", { title: 'View CSV', rows : csvRows, fileName: req.query.file });
+        });
+    });
+}
+
+exports.upload = function (req, res){
+    let item = fs.readFileSync(req.body.path,'utf8');
+    let key = req.body.path.split('/').pop();
+    let params = {
+        Bucket: myBucketName,
+        Key: key,
+        Body: item
+    };
+  
+    s3.putObject(params, function (err, data) {
+        if (err) {
+            console.log("Error uploading data: ", err);
+            let error = new Error('Error uploading');
+            error.httpStatusCode = 400;
+            // return next(error);
+            return res.send({ "error": error });
+        }
+        console.log(`Successfully uploaded data to ${myBucketName}/${key}`, data);
+        fs.unlinkSync(req.body.path);
+        res.redirect("/");
+    });
+};
